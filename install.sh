6ded60b62811e857589f638e6cd1aed2f41beede
jamesqj <271686059@qq.com>
2020-02-21 21:28:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash -e
-apt-get update && apt-get upgrade -y
-# install wget to download jexus, curl for healthcheck.
-apt-get install -y --no-install-recommends wget sqlite fontconfig xfonts-utils
-cd /usr
-wget https://linuxdot.net/down/jexus-6.2.x-x64.tar.gz 
-tar -zxvf jexus-6.2.x-x64.tar.gz 
-apt-get -y autoremove --purge wget 
-rm -rf /var/lib/apt/lists/* jexus-6.2.x-x64.tar.gz
+apt-get update \
+
+        && apt-get -y install wget sqlite fontconfig xfonts-utils \
+
+        && cd /usr \
+
+        && wget https://linuxdot.net/down/jexus-6.2-x64.tar.gz \
+
+        && tar -zxvf jexus-6.2-x64.tar.gz \
+
+        && apt-get -y autoremove --purge wget \
+
+        && rm -rf /var/lib/apt/lists/* jexus-6.1-x64.tar.gz
 
 
 
