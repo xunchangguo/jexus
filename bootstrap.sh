eb14c0d2c79d0201bdade9d9396e4e82843bf282
jamesqj <271686059@qq.com>
2020-02-21 15:02:11 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#service ssh start
+service ssh start
 /usr/jexus/jws start
 
 function stop_svc {
   /usr/jexus/jws stop
-  #service ssh stop
+  service ssh stop
   exit 0
 }
 
-#trap 'stop_svc' SIGTERM
+trap 'stop_svc' SIGTERM
 
 while true
 do
