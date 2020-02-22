aeb224005f98da525ba55f54860f0f8fe658957a
jamesqj <271686059@qq.com>
2020-02-22 20:30:42 +0800

    Update core3.1.sh

--- core3.1.sh
+++ core3.1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
+wget -q https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
 apt-get install apt-transport-https
 apt-get update
 apt-get install aspnetcore-runtime-3.1
