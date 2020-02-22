3f4d2d7a4d68bbc0e254f4f04ec6f5aa5c7d365d
jamesqj <271686059@qq.com>
2020-02-22 19:54:34 +0800

    Update core3.1.sh

--- core3.1.sh
+++ core3.1.sh
@@ -1,10 +1,4 @@
 #!/bin/bash -e
-
-dotnet_version=3.1.2 \
-    && curl -SL --output dotnet.tar.gz https://dotnetcli.azureedge.net/dotnet/Runtime/$dotnet_version/dotnet-runtime-$dotnet_version-linux-x64.tar.gz \
-    && dotnet_sha512='24a8154248ed2a95b1a6c8d72e4a92d1e898393d7950da2fa103e96684784cdfe74483f2a583f6c033d61c4f9dd090b0b6bcab2062bbac4fcd142dcb5685c7ff' \
-    && echo "$dotnet_sha512 dotnet.tar.gz" | sha512sum -c - \
-    && mkdir -p /usr/share/dotnet \
-    && tar -ozxf dotnet.tar.gz -C /usr/share/dotnet \
-    && rm dotnet.tar.gz \
-    && ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
+apt-get install apt-transport-https
+apt-get update
+apt-get install aspnetcore-runtime-3.1
