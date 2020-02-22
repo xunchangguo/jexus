1b83622fb240b1fab52f2bf6893334a8d2922a87
jamesqj <271686059@qq.com>
2020-02-22 20:46:33 +0800

    Update core3.1.sh

--- core3.1.sh
+++ core3.1.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
-wget -q https://packages.microsoft.com/config/ubuntu/19.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget -q https://packages.microsoft.com/config/debian/10/prod.list
+mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+chown root:root /etc/apt/sources.list.d/microsoft-prod.list
 apt-get install apt-transport-https
 apt-get update
 apt-get install aspnetcore-runtime-3.1
