f95a6d9e9c4237c40ec7506b92c10f318f6ba804
jamesqj <271686059@qq.com>
2020-02-22 10:46:50 +0800

    Create core3.1.sh

---
+++ core3.1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+ARG REPO=mcr.microsoft.com/dotnet/core/runtime-deps
+dotnet_version=3.1.2 \
+    && curl -SL --output dotnet.tar.gz https://dotnetcli.azureedge.net/dotnet/Runtime/$dotnet_version/dotnet-runtime-$dotnet_version-linux-x64.tar.gz \
+    && dotnet_sha512='24a8154248ed2a95b1a6c8d72e4a92d1e898393d7950da2fa103e96684784cdfe74483f2a583f6c033d61c4f9dd090b0b6bcab2062bbac4fcd142dcb5685c7ff' \
+    && echo "$dotnet_sha512 dotnet.tar.gz" | sha512sum -c - \
+    && mkdir -p /usr/share/dotnet \
+    && tar -ozxf dotnet.tar.gz -C /usr/share/dotnet \
+    && rm dotnet.tar.gz \
+    && ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
